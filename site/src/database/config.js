fb465e7517bb594b2850ed7abf2af392f91786e4
Torres5271 <gustavo.souza@sptech.school>
2023-06-05 19:41:05 -0300

    ajuste tamanho dashboard

--- site/src/database/config.js
+++ site/src/database/config.js
@@ -21,8 +21,8 @@ var sqlServerConfig = {
 var mySqlConfig = {
     host: "localhost",
     database: "FioreMove",
-    user: "torres",
-    password: "urubu100",
+    user: "aluno",
+    password: "sptech",
 };
 
 function executar(instrucao) {
